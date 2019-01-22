173eec3c94fa007c9f4bc4c8b09cce5d538527a6
Mary Matthews <matthewsmp@gmail.com>
2019-01-22 14:38:37 -0800

    setup schema file

--- schema.sql
+++ schema.sql
@@ -1,13 +1,28 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS your_last_grocery_list;
 
-CREATE DATABASE test;
+CREATE DATABASE your_last_grocery_list;
 
-USE test;
+USE your_last_grocery_list;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE lists (
+  id int NOT NULL AUTO_INCREMENT,
+  store_name varchar(75) NOT NULL,
+  user_id int NOT NULL references users(id),
+  PRIMARY KEY (ID)
+);
 
 CREATE TABLE items (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
+  list_id int NOT NULL references lists(id),
+  need_to_buy BOOLEAN NOT NULL,
+  category varchar(50) NOT NULL,
   PRIMARY KEY (ID)
 );
 
