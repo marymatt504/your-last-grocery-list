b377ad86925385498daed20c267727d2c7e1a11c
Mary Matthews <matthewsmp@gmail.com>
2019-01-22 15:41:40 -0800

    setup db connection file

--- database/index.js
+++ database/index.js
@@ -1,20 +1,64 @@
-var mysql = require('mysql');
+const db = './credentials.js';
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'FILL_ME_IN',
-  database: 'test'
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: db.user,
+  password: db.password,
+  // host: 'localhost',
+  // port: 5432,
+  database: 'your_last_grocery_list',
 });
 
-var selectAll = function (callback) {
-  connection.query('SELECT * FROM items', function (err, results, fields) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log('error from database/index.js line 15', err);
+  } else {
+    console.log(null, res);
+  }
+  pool.end();
+});
 
-module.exports.selectAll = selectAll;
+const client = new Client({
+  user: db.user,
+  password: db.password,
+  // host: 'localhost',
+  // port: 5432,
+  database: 'your_last_grocery_list',
+});
+client.connect();
+
+//***** DATABASE METHODS ***/
+
+// const getAll = (actor, callback) => {
+
+//   const query = {
+//     text: 'SELECT * FROM movies WHERE actor = $1',
+//     values: [actor],
+//   };
+
+//   client.query(query, (error, results) => {
+//     if (error) {
+//       callback(error);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
+
+// const cacheMovies = (title, year, image, actor, callback) => {
+//   const query = {
+//     text: 'INSERT INTO movies(title, year, image, actor) VALUES($1, $2, $3, $4)',
+//     values: [title, year, image, actor],
+//   };
+
+//   client.query(query, (error, results) => {
+//     if (error) {
+//       callback(error);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+
+// };
+// module.exports = { getAll, cacheMovies };
--- database/schema.sql
+++ database/schema.sql
@@ -1,25 +1,25 @@
 DROP DATABASE IF EXISTS your_last_grocery_list;
 
-CREATE DATABASE your_last_grocery_list;
+CREATE DATABASE your_last_grocery_list with owner = marymatthews;
 
-USE your_last_grocery_list;
+\connect your_last_grocery_list;
 
 CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   username varchar(50) NOT NULL,
   password varchar(50) NOT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE lists (
-  id int NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   store_name varchar(75) NOT NULL,
   user_id int NOT NULL references users(id),
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE items (
-  id int NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   list_id int NOT NULL references lists(id),
   need_to_buy BOOLEAN NOT NULL,
   category varchar(50) NOT NULL,
